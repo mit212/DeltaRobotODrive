aadddaeec81a396dde9f56cf63147ff0f92b4389
Jerry Ng <jerryng500@hotmail.com>
2019-04-16 18:05:45 -0400

    Added ik closed form solution. Added ik velocity solver.

--- kinematicsSolver.py
+++ kinematicsSolver.py
@@ -84,7 +84,85 @@ class deltaSolver(object):
 		self.fig = plt.figure()
 
 		self.plot((xx,yy,zz))
-	
+
+	def solveTheta1(self, position):
+		#Takes in an argument that is position class
+		#Solves for Theta1
+		E1 = 2*self.L*(position.y+self.a)
+		F1 = 2*position.z*self.L
+		G1 = position.x**2 + position.y**2 + position.z**2 + self.a**2 + self.L**2 + 2*position.y*self.a - self.l**2
+
+		return self.angleSolver(E1, F1, G1, 1)
+
+	def solveTheta2(self, position):
+		E2 = -self.L*(sqrt(3) * (position.x + self.b) + position.y + self.c)
+		F2 = 2*position.z*self.L
+		G2 = position.x**2 + position.y**2 + position.z**2 + self.b**2 + self.c**2 + self.L**2 + 2*(position.x*self.b + position.y*self.c) - self.l**2
+
+		return self.angleSolver(E2, F2, G2, 2)
+
+	def solveTheta3(self, position):
+		E3 = self.L * (sqrt(3) * (position.x - self.b) - position.y - self.c)
+		F3 = 2*position.z*self.L
+		G3 = position.x**2 + position.y**2 + position.z**2 + self.b**2 + self.c**2 + self.L**2 + 2*(-position.x*self.b + position.y*self.c) - self.l**2
+
+		return self.angleSolver(E3, F3, G3, 3)
+
+	def angleSolver(self, E, F, G, thetaID):
+		t1 = (-F + sqrt(E**2 + F**2 - G**2))/(G - E)
+		t2 = (-F - sqrt(E**2 + F**2 - G**2))/(G - E)
+		thetaPossible1 = 2*arctan(t1)
+		thetaPossible2 = 2*arctan(t2)
+
+		if(thetaID == 1):
+			currTheta = self.currTheta1
+		
+		elif(thetaID == 2):
+			currTheta = self.currTheta2
+
+		elif(thetaID == 3):
+			currTheta = self.currTheta3
+
+		#calculate the difference between the possible angles that solves the quadratic with current angle. 
+		thetaDiff1 = thetaPossible1 - self.currTheta1
+		thetaDiff2 = thetaPossible2 - self.currTheta2
+
+		#return the theta that is closest to the current theta
+		if(abs(thetaDiff1) < abs(thetaDiff2)):
+			return thetaPossible1
+
+		else:
+			return thetaPossible2
+
+
+	def velIKSolver(self, inputVec):
+		#inputVec is a vector in meters per second. [xdot, ydot, zdot]
+		a11 = self.x
+		a12 = self.y+self.a + self.L*cos(self.currTheta1)
+		a13 = self.z + self.L*sin(self.currTheta1)
+
+		a21 = 2*(self.x+self.b) - sqrt(3)*self.L*cos(self.currTheta2)
+		a22 = 2*(self.y + self.c) - self.L*cos(self.currTheta2)
+		a23 = 2*(self.z + self.L*sin(self.currTheta2))
+
+		a31 = 2*(self.x - self.b) + sqrt(3)*self.L*cos(self.currTheta3)
+		a32 = 2*(self.y + self.c) - self.L*cos(self.currTheta3)
+		a33 = 2*(self.z + self.L*sin(self.currTheta3))
+		A = np.matrix([[a11, a12, a13], 
+			 [a21, a22, a23], 
+			 [a31, a32, a33]])
+		b11 = L*((self.y + self.a)*sin(self.currTheta1) - self.z*cos(self.currTheta1))
+		b22 = -L*((sqrt(3)*(self.x + self.b) + self.y + self.c)*sin(self.currTheta2) + 2*self.z*cos(self.currTheta2))
+		b33 = L*((sqrt(3)*(self.x-self.b) - self.y - self.c)*sin(self.currTheta3) - 2*self.z*cos(self.currTheta3))
+		B = np.matrix([[b11, 0, 0], 
+			[0,b22,0], 
+			[0,0, b33]])
+		AB = np.matmul(A,B)
+		thetadot = np.matmul(AB, inputVec)
+
+		#return a vector of the angle velocities. [omega1, omega2, omega3]
+		return thetadot
+		
 	def plot(self, pos = (0, 0, -500)):
 		(x, y, z) = pos
 		thts = self.ik(pos)
